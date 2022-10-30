32d57b4cdff54d1bcc761b7dd3a8eacf75688682
Joydeep Singha <joydeepsingha68@gmail.com>
2022-10-30 15:15:50 +0530

    feat: add demo link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,79 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Button from '../components/Button'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Button from "../components/Button";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div >
+    <div>
       <Head>
         <title>Clubsss: create web3 powered communities</title>
         <meta name="description" content="Create web3 powered communities" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-<div className='w-full fixed p-4 bg-black flex justify-between'>
-  <button>
-  <img src="/img/clubsss.svg" className="w-28 object-contain" />
-  </button>
-  <div className='flex items-center'>
-  <a href='https://discord.gg/CraUtnzK' target="_blank" rel="noopener noreferrer" className=' px-5 text-sm py-2 flex justify-center font-bold rounded-md justify-self-end w-fit cursor-pointer '>Discord</a>
-  <a href='https://twitter.com/_clubsss' target="_blank" rel="noopener noreferrer" className=' px-5 text-sm py-2 flex justify-center font-bold rounded-md justify-self-end w-fit cursor-pointer '>Twitter</a>
-
-  </div>
-</div>
- <main className='bg-black w-full h-full min-h-screen flex overflow-hidden items-center justify-center'>
-
-  <div className='flex flex-col justify-center items-center'>
-
-  <h1     data-aos-duration="2000"
- data-aos="fade-right" className='text-4xl md:text-5xl text-white text-center font-bold mb-1'>Building an <br/> online community? 👀</h1>
-  <p data-aos-duration="2000"
- data-aos="fade-left" data-aos-delay="1000" className='w-full max-w-sm text-center mb-3 text-primary font-normal leading-[18px]'>Creating a web3 powered community(or dao) with <span className='font-black'>clubsss</span> is as simple as creating a subreddit. ✨</p>
-  <a href='https://discord.gg/CraUtnzK' target="_blank" rel="noopener noreferrer" className='bg-primary px-5 text-sm py-2 flex justify-center font-bold rounded-md justify-self-end w-fit cursor-pointer  hover:brightness-90'>Join Discord 👉</a>
-  </div>
- </main>
+      <div className="w-full fixed p-4 bg-black flex justify-between">
+        <button>
+          <img src="/img/clubsss.svg" className="w-28 object-contain" />
+        </button>
+        <div className="flex items-center">
+          <a
+            href="https://discord.gg/CraUtnzK"
+            target="_blank"
+            rel="noopener noreferrer"
+            className=" px-5 text-sm py-2 flex justify-center font-bold rounded-md justify-self-end w-fit cursor-pointer "
+          >
+            Discord
+          </a>
+          <a
+            href="https://twitter.com/_clubsss"
+            target="_blank"
+            rel="noopener noreferrer"
+            className=" px-5 text-sm py-2 flex justify-center font-bold rounded-md justify-self-end w-fit cursor-pointer "
+          >
+            Twitter
+          </a>
+          <a
+            href="https://www.app.clubsss.xyz/"
+            // target="_blank"
+            // rel="noopener noreferrer"
+            className="bg-primary px-5 text-sm py-2 flex justify-center font-bold rounded-md justify-self-end w-fit cursor-pointer  hover:brightness-90"
+          >
+            See Demo 👉
+          </a>
+        </div>
+      </div>
+      <main className="bg-black w-full h-full min-h-screen flex overflow-hidden items-center justify-center">
+        <div className="flex flex-col justify-center items-center">
+          <h1
+            data-aos-duration="2000"
+            data-aos="fade-right"
+            className="text-4xl md:text-5xl text-white text-center font-bold mb-1"
+          >
+            Building an <br /> online community? 👀
+          </h1>
+          <p
+            data-aos-duration="2000"
+            data-aos="fade-left"
+            data-aos-delay="1000"
+            className="w-full max-w-sm text-center mb-3 text-primary font-normal leading-[18px]"
+          >
+            Creating a web3 powered community(or dao) with{" "}
+            <span className="font-black">clubsss</span> is as simple as creating
+            a subreddit. ✨
+          </p>
+          <a
+            href="https://www.app.clubsss.xyz/"
+            // target="_blank"
+            // rel="noopener noreferrer"
+            className="bg-primary px-5 text-sm py-2 flex justify-center font-bold rounded-md justify-self-end w-fit cursor-pointer  hover:brightness-90"
+          >
+            See Demo 👉
+          </a>
+        </div>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
